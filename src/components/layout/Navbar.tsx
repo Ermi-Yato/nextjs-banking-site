b09ed4e13cfa238fd8ae61a6232adff6912c6bd9
Ermiyas <ermiyasbi007@gmail.com>
2025-10-17 10:51:26 +0300

    mobile nav bar and click events

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -13,47 +13,74 @@ import { Button } from "../ui/Button"
 import abstractDesign from "@/assets/images/mobile-abstract.svg"
 import { usePathname } from "next/navigation"
 import { navLinks } from "@/constants/navLinks"
+import { useState } from "react"
 
 export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   const pathName = usePathname()
 
   return (
     <header className="py-10 relative">
       <Image src={abstractDesign} alt="" className="absolute top-0 left-0 -z-10" />
       <div className="container">
-        <div className="bg-[#1c1c1c] border border-[#262626] rounded-full flex justify-between items-center h-16 px-4 pl-6">
-          <Link href="/">
-            <Image src={logoMobile} alt="mobile logo" className="md:hidden" />
-            <Image src={logoDesktop} alt="mobile logo" className="hidden md:block" />
-          </Link>
-          <div className="hidden lg:block">
-            <nav className="flex gap-2">
-              {navLinks.map((link) => {
-                const isActive: boolean = pathName === link.href
-                return (
-                  <>
+        <div className="bg-[#1c1c1c] border border-[#262626] rounded-4xl flex flex-col gap-4">
+          <div className="flex justify-between items-center h-16 px-4 pl-6">
+            <Link href="/">
+              <Image src={logoMobile} alt="mobile logo" className="md:hidden" />
+              <Image src={logoDesktop} alt="mobile logo" className="hidden md:block" />
+            </Link>
+            <div className="hidden lg:block">
+              <nav className="flex gap-2">
+                {navLinks.map((link) => {
+                  const isActive: boolean = pathName === link.href
+                  return (
                     <Link href={link.href} key={link.href}>
                       <span className={`text-sm px-4 py-3 rounded-3xl ${isActive ? "bg-[#262626]" : ""}`}>{link.name}</span>
                     </Link>
-                  </>
+                  )
+                })}
+              </nav>
+            </div>
+
+
+            <div className="hidden md:block">
+              <div className="flex gap-2">
+                <Link href="/signup">
+                  <Button btnType="secondary" size="md" dropShadow="yes">Sign Up</Button>
+                </Link>
+                <Link href="/login">
+                  <Button btnType="primary" size="md" dropShadow="yes">Login</Button>
+                </Link>
+              </div>
+            </div>
+            <button
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="w-14 h-10 bg-[#CAFF33] inline-flex items-center rounded-full justify-center md:hidden">
+              <Image src={hamburger} alt="menu icon" />
+            </button>
+          </div>
+
+          {isMenuOpen && (
+            <div className="p-6">
+
+              {navLinks.map((link) => {
+                const isActive = pathName === link.href
+                return (
+                  <Link
+                    key={link.href}
+                    href={link.href}
+                    className={`block text-sm py-4 rounded-xl text-center ${isActive ? "bg-[#262626] text-[#CAFF33]" : "text-[#B3B3B3]"
+                      }`}
+                    onClick={() => setIsMenuOpen(false)} // close menu after navigation
+                  >
+                    {link.name}
+                  </Link>
                 )
               })}
-            </nav>
 
-          </div>
-          <div className="hidden md:block">
-            <div className="flex gap-2">
-              <Link href="/signup">
-                <Button btnType="secondary" size="md" dropShadow="yes">Sign Up</Button>
-              </Link>
-              <Link href="/login">
-                <Button btnType="primary" size="md" dropShadow="yes">Login</Button>
-              </Link>
             </div>
-          </div>
-          <div className="w-14 h-10 bg-[#CAFF33] inline-flex items-center rounded-full justify-center md:hidden">
-            <Image src={hamburger} alt="menu icon" />
-          </div>
+          )}
+
         </div>
       </div>
     </header>
